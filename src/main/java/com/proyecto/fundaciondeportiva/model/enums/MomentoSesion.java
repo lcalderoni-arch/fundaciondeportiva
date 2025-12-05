2be913ccd78da44c30014e1f97f84c4e55d33d9b
Alan Puma Quispe <alan.puma@autonoma.pe>
2025-12-05 17:04:44 -0500

    ACTUALIZACION UNIVERSIDAD BACKEND, ARREGLANDO ERROR v4

--- src/main/java/com/proyecto/fundaciondeportiva/model/enums/MomentoSesion.java
+++ src/main/java/com/proyecto/fundaciondeportiva/model/enums/MomentoSesion.java
@@ -5,7 +5,7 @@ package com.proyecto.fundaciondeportiva.model.enums;
  * Basado en: ENUM('ANTES','DURANTE','DESPUES')
  */
 public enum MomentoSesion {
-    EXPLORA,
-    ESTUDIA,
-    APLICA
+    ANTES,
+    DURANTE,
+    DESPUES
 }
