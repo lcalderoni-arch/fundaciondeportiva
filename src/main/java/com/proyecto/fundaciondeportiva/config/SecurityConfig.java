cb84909290c2ab9c734da860a9b2476d3b01e53b
lcalderoni-arch <lcalderoni@autonoma.edu.pe>
2025-12-06 08:10:36 -0500

    ARREGLO DE ASISTENCIA DOCENTE v26

--- src/main/java/com/proyecto/fundaciondeportiva/config/SecurityConfig.java
+++ src/main/java/com/proyecto/fundaciondeportiva/config/SecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -65,7 +66,8 @@ public class SecurityConfig {
                                                    AuthenticationProvider authenticationProvider,
                                                    JwtAuthenticationFilter jwtAuthFilter) throws Exception {
         http
-                .cors(cors -> cors.configurationSource(corsConfigurationSource())) // ⭐ Usa el bean explícitamente
+                .cors(Customizer.withDefaults())
+                // ⭐ Usa el bean explícitamente
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
