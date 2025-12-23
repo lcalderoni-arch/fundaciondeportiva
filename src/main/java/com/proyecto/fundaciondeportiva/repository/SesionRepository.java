1d5cc9d22bbcefe3c1a43edc440df7f7e013dc33
lcalderoni-arch <lcalderoni@autonoma.edu.pe>
2025-12-22 19:06:49 -0500

    Update SesionRepository.java

--- src/main/java/com/proyecto/fundaciondeportiva/repository/SesionRepository.java
+++ src/main/java/com/proyecto/fundaciondeportiva/repository/SesionRepository.java
@@ -13,12 +13,12 @@ public interface SesionRepository extends JpaRepository<Sesion, Long> {
 
     List<Sesion> findBySeccion_IdOrderByFechaAsc(Long seccionId);
 
-    // ⭐ sesiones de un día
+    // sesiones de un día
     List<Sesion> findByFecha(LocalDate fecha);
 
-    // ⭐ TODAS las sesiones de un profesor (por id del usuario profesor)
+    // TODAS las sesiones de un profesor (por id del usuario profesor)
     List<Sesion> findBySeccion_Profesor_IdOrderByFechaAsc(Long profesorId);
 
-    // ⭐ TODAS las sesiones de un alumno (por id del usuario alumno vía matrícula)
+    // TODAS las sesiones de un alumno (por id del usuario alumno vía matrícula)
     List<Sesion> findBySeccion_Matriculas_Alumno_IdOrderByFechaAsc(Long alumnoId);
 }
